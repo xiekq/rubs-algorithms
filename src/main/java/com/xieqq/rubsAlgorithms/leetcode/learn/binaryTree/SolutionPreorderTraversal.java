3022e69adf5f0f2159b4d794b6f2767049fa17d3
xiekeqing <xiekeqing@mbcloud.com>
2020-06-01 23:09:58 +0800

    提交二叉树前中后序遍历

---
+++ src/main/java/com/xieqq/rubsAlgorithms/leetcode/learn/binaryTree/SolutionPreorderTraversal.java
@@ -0,0 +1,39 @@
+package com.xieqq.rubsAlgorithms.leetcode.learn.binaryTree;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 二叉树前中后序遍历
+ */
+public class SolutionPreorderTraversal {
+    List<Integer> orderList = new ArrayList<>();
+
+    public List<Integer> postorderTraversal(TreeNode root) {
+        if (null != root) {
+            postorderTraversal(root.left);
+            postorderTraversal(root.right);
+            orderList.add(root.val);
+        }
+        return orderList;
+    }
+
+    public List<Integer> inorderTraversal(TreeNode root) {
+        if (null != root) {
+            inorderTraversal(root.left);
+            orderList.add(root.val);
+            inorderTraversal(root.right);
+        }
+        return orderList;
+    }
+
+    public List<Integer> preorderTraversal(TreeNode root) {
+        if (null != root) {
+            orderList.add(root.val);
+            preorderTraversal(root.left);
+            preorderTraversal(root.right);
+        }
+        return orderList;
+    }
+
+}
---
+++ src/main/java/com/xieqq/rubsAlgorithms/leetcode/learn/binaryTree/TreeNode.java
@@ -0,0 +1,20 @@
+package com.xieqq.rubsAlgorithms.leetcode.learn.binaryTree;
+
+/**
+ * Definition for a binary tree node.
+ */
+public class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode() {
+    }
+
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+
+}
