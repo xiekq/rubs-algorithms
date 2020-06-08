95d7465180f0433dca4cabfe211b2f1b0edd228f
xiekeqing <xiekeqing@mbcloud.com>
2020-06-08 23:28:09 +0800

    是否存在路径之和等于给定值

---
+++ src/main/java/com/xieqq/rubsAlgorithms/leetcode/learn/binaryTree/SolutionHasPathSum.java
@@ -0,0 +1,23 @@
+package com.xieqq.rubsAlgorithms.leetcode.learn.binaryTree;
+
+public class SolutionHasPathSum {
+
+    public boolean hasPathSum(TreeNode root, int sum) {
+        return hasPathSum(root, sum, 0);
+    }
+
+    private boolean hasPathSum(TreeNode node, int sum, int preSum) {
+
+        if(node == null){
+            return false;
+        }
+
+        preSum += node.val;
+        if (null == node.left && null == node.right) {
+            return sum == preSum;
+        }
+
+        return hasPathSum(node.left, sum, preSum) || hasPathSum(node.right, sum, preSum);
+    }
+
+}
